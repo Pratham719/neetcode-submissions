86a960c5b7c99e5e14bb67070b0eb29f8c85ba91
Pratham719 <prathamrangoon2007@gmail.com>
2026-05-18 14:30:27 +0530

    Add: find-pivot-index - submission-5

---
+++ Data Structures & Algorithms/find-pivot-index/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0
+        total = sum(nums)
+
+        for i in range(len(nums)):
+            current = nums[i]
+            right = total - left - current
+            if right == left:
+                return i
+            left += current
+        return -1
+
+
+sol = Solution()
+arr = [1, 7, 3, 6, 5, 6]
+print(sol.pivotIndex(arr))
