663a6dd0d967b52ecd85c997f1b7f6adf9478630
Pratham719 <prathamrangoon2007@gmail.com>
2026-05-18 15:42:50 +0530

    Add: squares-of-a-sorted-array - submission-2

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result = [0] * len(nums)
+        l = 0
+        r = len(nums) - 1
+        pos = r
+
+        while l<=r:
+            left_square=nums[l]*nums[l]
+            right_square=nums[r]*nums[r]
+
+            if left_square>right_square:
+                result[pos]=left_square
+                l+=1
+            else:
+                result[pos]=right_square
+                r-=1
+            pos-=1
+        return result
+
